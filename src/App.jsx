2c974b768a83dfe959fbde632f3a5113d8459503
Affandi99-bit <affandi.blck@gmail.com>
2025-10-09 14:48:40 +0700

    bismillah akhir tahun jadi

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-dark'>
+      <p className='text-light font-head text-5xl font-bold'>Head</p>
+      <p className='text-accent text-xs font-body tracking-tighter'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla deleniti optio consequatur quam, quibusdam dolor quaerat impedit vel, enim ut atque inventore eum. Reprehenderit nesciunt ipsum earum libero fugit cupiditate!</p>
+    </div>
+  )
+}
+
+export default App
